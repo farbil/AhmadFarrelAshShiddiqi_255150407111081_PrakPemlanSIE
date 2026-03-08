8f3d59b64cee847e046a1ee96427cf35b02fdb8d
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2026-03-08 16:03:38 +0700

    Menambahkan modul 1 - class and object

---
+++ Modul 1/modul-1-class-and-object/src/Main.java
@@ -0,0 +1,18 @@
+//public class Main {
+//    public static void main(String[] args) {
+////        variabel primitif dan referensi
+////        int umur1 = 3;
+////        int umur2 = umur1;
+////        umur2 = 5;
+////
+////        System.out.println("Umur kucing 1" + umur1);
+////        System.out.println("Umur kucing 2" + umur2);
+//
+//        int umur1 = 3;
+//        int umur2 = umur1;
+//        umur2 = 5;
+//
+//        System.out.println("Umur kucing 1 adalah " + umur1);
+//        System.out.println("Umur kucing 2 adalah " + umur2);
+//    }
+//}
---
+++ Modul 1/modul-1-class-and-object/src/Mobil.java
@@ -0,0 +1,23 @@
+public class Mobil {
+    private String nopol;
+    private String warna;
+    private String merk;
+    private int kecepatan;
+    public void setNopol(String n) {
+        nopol = n;
+    }
+    public void setWarna(String s) {
+        warna = s;
+    }
+    public void setMerk(String m) {
+        merk = m;
+    }
+    public void setKecepatan(int k) {
+        kecepatan = k;
+    }
+    public void display() {
+        System.out.println("Mobil bermerk " + merk);
+        System.out.println("bernomor polisi " + nopol);
+        System.out.println("serta memililki warna " + warna);
+        System.out.println("bergerak dengan kecepatan " + kecepatan + " kpj"); }
+}
