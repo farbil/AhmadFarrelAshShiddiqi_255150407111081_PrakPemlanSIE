c96a1f7f4409ccda1129bcefaa73b1195f12253f
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2026-03-25 17:31:56 +0700

    Menambahkan soal modul 2 - member static dan non static

---
+++ Modul 2/modul-2-member-static-dan-non-static/src/Lingkaran.java
@@ -0,0 +1,12 @@
+public class Lingkaran {
+    public static float PI = 3.14f;
+    public float r;
+
+    public Lingkaran(float r) {
+        this.r = r;
+    }
+
+    public float luas() {
+        return Lingkaran.PI * r * r;
+    }
+}
