7ffe8dc2aa73201b52d2fe802eec501d6149df2e
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2026-03-25 17:41:21 +0700

    Menghapus modifier static pada atribut r

--- Modul 2/modul-2-member-static-dan-non-static/src/Lingkaran.java
+++ Modul 2/modul-2-member-static-dan-non-static/src/Lingkaran.java
@@ -1,12 +1,12 @@
 public class Lingkaran {
     public static float PI = 3.14f;
-    static public float r;
+    public float r;
 
     public Lingkaran(float r) {
         this.r = r;
     }
 
-    public float luas() {
+    static public float luas() {
         return Lingkaran.PI * r * r;
     }
 }
