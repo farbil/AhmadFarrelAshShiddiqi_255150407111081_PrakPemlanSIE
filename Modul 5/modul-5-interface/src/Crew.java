536d62b7eb7b77dba84b927451dc5c085a7cddc1
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2026-04-08 22:25:04 +0700

    Menambahkan soal modul 5 - interface - Crew

---
+++ Modul 5/modul-5-interface/src/Crew.java
@@ -0,0 +1,25 @@
+public class Crew implements ICrew {
+
+    private String name;
+
+    public Crew(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void doWork() {
+        System.out.println("Crew " + this.name + " is doing work.");
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    public void callMeeting() {
+        System.out.print(this.name + " found a corpse ");
+        System.out.print("and calls a meeting. ");
+        System.out.println("Let's find the impostor!");
+    }
+
+}
