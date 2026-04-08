96463103aded729d4204a2c116f6d4ed3dcefc6b
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2026-04-08 23:27:39 +0700

    Memindahkan posisi method kick()

--- Modul 5/modul-5-interface/src/Crew.java
+++ Modul 5/modul-5-interface/src/Crew.java
@@ -16,14 +16,15 @@ public class Crew implements ICrew {
         return this.name;
     }
 
+    @Override
+    public void kick() {
+        System.out.println(this.name + " was ejected.");
+    }
+
     public void callMeeting() {
         System.out.print(this.name + " found a corpse ");
         System.out.print("and calls a meeting. ");
         System.out.println("Let's find the impostor!");
-    }
-
-    @Override public void kick() {
-        System.out.println(this.name + " was ejected.");
     }
 
 }
