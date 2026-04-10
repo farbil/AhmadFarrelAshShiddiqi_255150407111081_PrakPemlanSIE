fb82d0a7b2328a81222400afc60ccb0c313bf1cb
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2026-04-10 23:32:09 +0700

    Menambahkan permintaan soal no 3

--- Modul 6/modul-6-konsep-Node-dalam-Linked-List/src/Node.java
+++ Modul 6/modul-6-konsep-Node-dalam-Linked-List/src/Node.java
@@ -3,6 +3,11 @@ public class Node {
     Node next;
 
     public static void main(String[] args) {
-        
+        Node node = new Node();
+        node.data = "A";
+
+        System.out.println("node    : " + node);
+        System.out.println("data    : " + node.data);
+        System.out.println("pointer : " + node.next);
     }
 }
