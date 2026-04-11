5bcb5a0ca0610445297ce9d322c90f0a2820e54a
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2026-04-11 18:25:34 +0700

    Membuat class SingleLinkedList dan menambahkan kode program soal no 1 Operasi dalam Single Linked-List

---
+++ Modul 6/SingleLinkedListProject/src/SingleLinkedList.java
@@ -0,0 +1,32 @@
+public class SingleLinkedList {
+
+    Node head, tail;
+    int size = 0;
+
+    void init() { head = null; }
+    boolean isEmpty() { return size == 0; }
+    int size() { return size; }
+
+    void addFirst(Node data) {
+        if (isEmpty()) {
+            head = data;
+            tail = data;
+        } else {
+            data.next = head;
+            head = data;
+        }
+        size++;
+    }
+
+    void addLast(Node data) {
+        if (isEmpty()) {
+            head = data;
+            tail = data;
+        } else {
+            tail.next = data;
+            tail = data;
+        }
+        size++;
+    }
+
+}
