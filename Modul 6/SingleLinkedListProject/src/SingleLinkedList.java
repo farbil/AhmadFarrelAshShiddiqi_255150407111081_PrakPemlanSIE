64291b6c007a912e7a19079ec6e35df533bb7efd
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2026-04-15 14:24:07 +0700

    Menambahkan method untuk mencetak list

--- Modul 6/SingleLinkedListProject/src/SingleLinkedList.java
+++ Modul 6/SingleLinkedListProject/src/SingleLinkedList.java
@@ -29,12 +29,22 @@ public class SingleLinkedList {
         size++;
     }
 
+    static void cetakList(SingleLinkedList list) {
+        Node current = list.head;
+        while(current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
     public static void main(String[] args) {
 
         SingleLinkedList list = new SingleLinkedList();
         list.addLast(new Node("A"));
         list.addLast(new Node("B"));
         list.addLast(new Node("D"));
+        cetakList(list);
 
     }
 
