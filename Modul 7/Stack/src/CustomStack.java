e533d16cee7c5b44af08ec82d953c30ff8b9c309
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2026-04-22 18:01:53 +0700

    Membuat method peek() untuk melihat data yang terdapat di depan/atas dari Stack tanpa mengeluarkannya dari Stack dan pemanggilannya

--- Modul 7/Stack/src/CustomStack.java
+++ Modul 7/Stack/src/CustomStack.java
@@ -23,10 +23,18 @@ public class CustomStack {
         return top == null;
     }
 
+    public int peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty.");
+        }
+        return top.data;
+    }
+
     public static void main(String[] args) {
         CustomStack custom = new CustomStack();
         custom.push(1);
         custom.push(2);
+        System.out.println("Top saat ini (peek): " + custom.peek());
         custom.push(3);
         System.out.print("Custom Stack: ");
         while (!custom.isEmpty()) {
