eb1f4e88be03ec11b78a51ecd274d234bb958215
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2026-04-28 22:48:09 +0700

    Membuat BinaryTree.java

---
+++ Modul 8/TreeApp/src/BinaryTree.java
@@ -0,0 +1,34 @@
+public class BinaryTree {
+
+    Node root;
+
+    public void add(int data) {
+        if (root == null) {
+            root = new Node(data);
+            return;
+        }
+        root.add(data);
+    }
+
+    public void preTraverse(Node node) {
+        if (node == null) return;
+        System.out.printf("[%d]", node.data);
+        preTraverse(node.left);
+        preTraverse(node.right);
+    }
+
+    public void inTraverse(Node node) {
+        if (node == null) return;
+        inTraverse(node.left);
+        System.out.printf("[%d]", node.data);
+        inTraverse(node.right);
+    }
+
+    public void postTraverse(Node node) {
+        if (node == null) return;
+        postTraverse(node.left);
+        postTraverse(node.right);
+        System.out.printf("[%d]", node.data);
+    }
+
+}
