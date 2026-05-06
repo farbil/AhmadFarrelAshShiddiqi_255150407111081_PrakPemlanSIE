c51f9065887274aa3674108a0cd65955e7f3e987
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2026-05-06 21:37:58 +0700

    Menjawab soal no 6

--- Modul 9/sorting/src/Sort.java
+++ Modul 9/sorting/src/Sort.java
@@ -3,52 +3,51 @@ import java.util.Arrays;
 public class Sort {
 
     public static int[] bubble(int[] list) {
+        int tukarAtauGeser = 0;
         for (int i = 0; i < list.length - 1; i++) {
             for (int k = i + 1; k < list.length; k++) {
-                System.out.printf("%d:%d\n", list[k], list[i]);
                 if (list[k] > list[i]) {
                     int t = list[i];
                     list[i] = list[k];
                     list[k] = t;
+                    tukarAtauGeser++;
                 }
             }
         }
+        System.out.println("Jumlah tukar/geser Bubble: " + tukarAtauGeser);
         return list;
     }
 
     public static int[] insertion(int[] list) {
+        int tukarAtauGeser = 0;
         for (int i = 1; i < list.length; ++i) {
             int key = list[i];
             int k = i - 1;
-
-            /*
-             * Move elements of arr[0..i-1], that are greater than key,
-             * to one position ahead of their current position
-             */
             while (k >= 0 && list[k] < key) {
                 list[k + 1] = list[k];
                 k = k - 1;
+                tukarAtauGeser++;
             }
             list[k + 1] = key;
         }
+        System.out.println("Jumlah tukar/geser Insertion: " + tukarAtauGeser);
         return list;
     }
 
     public static int[] selection(int[] list) {
-        // One by one move boundary of unsorted subarray
+        int tukarAtauGeser = 0;
         for (int i = 0; i < list.length - 1; i++) {
-            // Find the minimum element in unsorted array
             int minIndex = i;
             for (int k = i + 1; k < list.length; k++) {
                 if (list[k] > list[minIndex])
                     minIndex = k;
             }
-            // Swap the found minimum element
-            // with the first element
             int t = list[minIndex];
             list[minIndex] = list[i];
             list[i] = t;
+            tukarAtauGeser++;
         }
+        System.out.println("Jumlah tukar/geser Selection: " + tukarAtauGeser);
         return list;
     }
 
@@ -60,9 +59,9 @@ public class Sort {
         System.out.println("Array sebelum diurutkan:");
         System.out.println(Arrays.toString(data));
 
-        int[] sorted = bubble(data);
+        // int[] sorted = bubble(data);
         // int[] sorted = insertion(data);
-        // int[] sorted = selection(data);
+        int[] sorted = selection(data);
         System.out.println("Array setelah diurutkan:");
         System.out.println(Arrays.toString(sorted));
     }
