013cefa0a11cf8c01ea53988787ac8f7ad6d36db
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2026-05-06 22:36:33 +0700

    Menjawab soal no 6 dengan menambahkan deklarasi variabel lokal dan meletakkan statement

--- Modul 9/searching/src/Search.java
+++ Modul 9/searching/src/Search.java
@@ -1,34 +1,38 @@
 public class Search {
 
     public static int linearSearch(int[] arr, int search) {
+        int check = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == search) return i;
+            check++;
+            if (arr[i] == search) {
+                System.out.println("Linear Search memeriksa sebanyak: " + check + " kali");
+                return i;
+            }
         }
+        System.out.println("Linear Search memeriksa sebanyak: " + check + " kali");
         return -1;
     }
-    public static int binarySearch(int[] arr, int search ) {
+    public static int binarySearch(int[] arr, int search) {
+        int check = 0;
         int low = 0, high = arr.length - 1;
         while (low <= high) {
+            check++;
             int mid = low + (high - low) / 2;
-            if (arr[mid] == search) return mid;
+            if (arr[mid] == search) {
+                System.out.println("Binary Search memeriksa sebanyak: " + check + " kali");
+                return mid;
+            }
             if (arr[mid] < search) low = mid + 1;
             else high = mid - 1;
         }
+        System.out.println("Binary Search memeriksa sebanyak: " + check + " kali");
         return -1;
     }
     public static void main(String[] args) {
         int[] arrayTerurutAscending = {2, 5, 8, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 55, 58, 62, 65, 68, 72, 75, 78, 81, 85, 88, 92, 95};
 
+        System.out.println("Mencari elemen 88");
         int indeksLinear = linearSearch(arrayTerurutAscending, 88);
-        System.out.println("Nilai indeks dari Linear Search (88): " + indeksLinear);
-
-        int indeksLinearTidakDitemukan = linearSearch(arrayTerurutAscending, 100);
-        System.out.println("Nilai indeks dari Linear Search (100): " + indeksLinearTidakDitemukan);
-
         int indeksBinary = binarySearch(arrayTerurutAscending, 88);
-        System.out.println("Nilai indeks dari Binary Search (88): " + indeksBinary);
-
-        int indeksBinaryTidakDitemukan = binarySearch(arrayTerurutAscending, 100);
-        System.out.println("Nilai indeks dari Binary Search (100): " + indeksBinaryTidakDitemukan);
     }
 }
